d08904a98a25d5c6372e30dac815b148c4a4e98f
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2021-10-23 19:26:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class Auto:
             return "Las piezas no son originales"
         else:
             for i in self.asientos:
-                if (i.registro!=self.registro and i!=None):
+                if (i!=None and i.registro!=self.registro):
                     return "Las piezas no son originales"
             return "Auto original"
 
