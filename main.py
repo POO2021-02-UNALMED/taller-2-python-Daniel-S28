1bba6d7368ad9aab9e4fe0a9f153da16cd5b6028
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2021-10-23 19:21:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ class Asiento:
 
 class Auto:
     cantidadCreados=0
-    def __init__(self,precio,asientos,marca,motor,registro):
+    def __init__(self,modelo,precio,asientos,marca,motor,registro):
+        self.modelo=modelo
         self.precio=precio
         self.asientos=asientos
         self.marca=marca
